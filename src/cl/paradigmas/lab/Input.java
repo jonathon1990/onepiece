372628779ccc280b8245e0992751f4fd4e37278c
Alumno <Alumno@EQ42-LAB2>
2014-04-21 19:53:27 -0300

    commit lab

---
+++ src/cl/paradigmas/lab/Input.java
@@ -0,0 +1,14 @@
+package cl.paradigmas.lab;
+
+public class Input {
+
+	private int valor;
+
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+
+	public int getValor() {
+		return valor;
+	}
+}
---
+++ src/cl/paradigmas/lab/Main.java
@@ -0,0 +1,16 @@
+package cl.paradigmas.lab;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Input input = new Input();
+		//input.setValor(5);
+		System.out.println("El valor de input es "+ input.getValor());
+
+	}
+
+}
+//Tipos de datos primitivos, int char todos los valores numericos, 
